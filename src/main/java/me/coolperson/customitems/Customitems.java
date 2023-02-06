91d17d57ddd4827851ae1fcf11b545cd1dc656cf
coolperson <thenewepicgamer@gmail.com>
2023-02-06 08:42:23 -0500

    First Commit

---
+++ src/main/java/me/coolperson/customitems/Customitems.java
@@ -0,0 +1,18 @@
+package me.coolperson.customitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class
+Customitems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
